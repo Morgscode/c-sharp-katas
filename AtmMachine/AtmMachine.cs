085600943e6ed959c9cbf3f7e7e79c0c42b2a656
Luke Morgan <luke@tjcuk.co.uk>
2023-09-22 22:30:02 +0100

    repo formatting

--- AtmMachine/AtmMachine.cs
+++ AtmMachine/AtmMachine.cs
@@ -3,67 +3,67 @@
 public class AtmMachine
 {
 
-	private Dictionary<int, int> NoteInventory { get;  }
-	private int TotalAvailableCash = 0;
+    private Dictionary<int, int> NoteInventory { get; }
+    private int TotalAvailableCash = 0;
 
-	public AtmMachine(Dictionary<int, int> inventory)
-	{
-		NoteInventory = inventory ?? throw new ArgumentNullException(nameof(inventory));
+    public AtmMachine(Dictionary<int, int> inventory)
+    {
+        NoteInventory = inventory ?? throw new ArgumentNullException(nameof(inventory));
 
-		foreach(var item in inventory)
-		{
-			int itemTotal = item.Key * item.Value;
-                TotalAvailableCash += itemTotal;
-		}
-	}
+        foreach (var item in inventory)
+        {
+            int itemTotal = item.Key * item.Value;
+            TotalAvailableCash += itemTotal;
+        }
+    }
 
-	public int GetTotalCashAvailable()
-	{
-		return TotalAvailableCash;
-	}
+    public int GetTotalCashAvailable()
+    {
+        return TotalAvailableCash;
+    }
 
-	public Dictionary<int, int> Withdraw(int amount)
-	{
-            if (amount <= 0) throw new ArgumentException("Invalid Input - Amount must be greater than 0");
+    public Dictionary<int, int> Withdraw(int amount)
+    {
+        if (amount <= 0) throw new ArgumentException("Invalid Input - Amount must be greater than 0");
 
-		if (amount % 5 != 0) throw new ArgumentException("Invalid Input - Amount cannot be dispensed");
+        if (amount % 5 != 0) throw new ArgumentException("Invalid Input - Amount cannot be dispensed");
 
-            if (TotalAvailableCash == 0) throw new Exception("There is no cash available at this atm");
+        if (TotalAvailableCash == 0) throw new Exception("There is no cash available at this atm");
 
-		if (amount > TotalAvailableCash) throw new ArgumentException("That amount is not available to dispense");
+        if (amount > TotalAvailableCash) throw new ArgumentException("That amount is not available to dispense");
 
-            return GetNotes(amount);
-	}
+        return GetNotes(amount);
+    }
 
-	private Dictionary<int, int> GetNotes(int amount)
-	{
+    private Dictionary<int, int> GetNotes(int amount)
+    {
 
-		var output = new Dictionary<int, int> { };
+        var output = new Dictionary<int, int> { };
 
-            foreach (var note in NoteInventory)
+        foreach (var note in NoteInventory)
+        {
+            if (amount == 0) break;
+
+            // if we don't  have any of the notes left, continue
+            if (note.Value == 0) continue;
+
+            // calc how many of this note to dispense
+            int count = amount / note.Key;
+            if (count > 0)
             {
-                if (amount == 0) break;
-
-			// if we don't  have any of the notes left, continue
-			if (note.Value == 0) continue;
-
-			// calc how many of this note to dispense
-                int count = amount / note.Key;
-                if (count > 0)
-                {
-				// add the quantity of notes to the output
-                    output.Add(note.Key, count);
-                    // update the amount
-                    amount -= count * note.Key;
-				// subtract from total cash available
-				TotalAvailableCash -= amount;
-				// update inventory count of note
-				NoteInventory[note.Key] -= count;
-                }
+                // add the quantity of notes to the output
+                output.Add(note.Key, count);
+                // update the amount
+                amount -= count * note.Key;
+                // subtract from total cash available
+                TotalAvailableCash -= amount;
+                // update inventory count of note
+                NoteInventory[note.Key] -= count;
             }
-
-            return output;
         }
 
+        return output;
     }
+
+}
 
