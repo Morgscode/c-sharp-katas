9f183efcb2a9963990f7f1d924dfcab917d7f7af
Luke Morgan <luke@tjcuk.co.uk>
2023-10-03 22:34:38 +0100

    string calc kata built and tested

---
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,85 @@
+﻿namespace StringCalculator.Tests;
+
+using System.Reflection;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void HasAddMethod()
+    {
+        // Arrange
+        Type type = typeof(Calculator);
+
+        // Act
+        var methodInfo = type.GetMethod("Add", BindingFlags.Public | BindingFlags.Instance);
+
+        // Assert
+        Assert.NotNull(methodInfo);
+    }
+
+    [Fact]
+    public void ReturnsZeroForEmptyString()
+    {
+        // Arrange
+        Calculator calculator = new Calculator();
+        // Act
+        int result = calculator.Add("");
+        // Assert
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("0", 0)]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("3", 3)]
+    [InlineData("4", 4)]
+    [InlineData("5", 5)]
+    [InlineData("6", 6)]
+    [InlineData("7", 7)]
+    [InlineData("8", 8)]
+    [InlineData("9", 9)]
+    [InlineData("10", 10)]
+    [InlineData("11", 11)]
+    [InlineData("111", 111)]
+    [InlineData("9000990", 9000990)]
+    public void HandlesOneNumber(string input, int output)
+    {
+        // Arrange
+        Calculator calculator = new Calculator();
+        // Act
+        int result = calculator.Add(input);
+        // Assert
+        Assert.Equal(output, result);
+    }
+
+    [Theory]
+    [InlineData("a")]
+    [InlineData("abc")]
+    [InlineData("stavvy")]
+    [InlineData("jkjskjskjsksj")]
+    [InlineData("1,jkjskjskjsksj")]
+    [InlineData("1,jkjskjskjsksj,3")]
+    public void ThrowsForInvalidInputs(string input)
+    {
+        // Arrange
+        Calculator calculator = new Calculator();
+        // Act
+        ArgumentException e = Assert.Throws<ArgumentException>(() => calculator.Add(input));
+    }
+
+    [Theory]
+    [InlineData("2,2", 4)]
+    [InlineData("3,1", 4)]
+    public void HandlesTwoNumbers(string input, int output)
+    {
+        // Arrange
+        Calculator calculator = new Calculator();
+        // Act
+        int result = calculator.Add(input);
+        // Assert
+        Assert.Equal(output, result);
+    }
+}
---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,45 @@
+﻿namespace StringCalculator;
+
+public class Calculator
+{
+    public Calculator()
+    {
+    }
+
+    public int Add(string input)
+    {
+        if (input == "") return 0;
+
+        if (input.Contains(",") == false)
+        {
+            return ParseStringToNumberOrFail(input);
+        }
+        else
+        {
+            string[] strings = input.Split(",");
+
+            if (strings.Length > 2) throw new ArgumentException($"Invalid input - {strings.Length} arguements passed in and this module expects 2");
+
+            int result = 0;
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                result += ParseStringToNumberOrFail(strings[i]);
+            }
+
+            return result;
+        }
+    }
+
+    private int ParseStringToNumberOrFail(string input)
+    {
+        if (int.TryParse(input, out int number))
+        {
+            return number;
+        }
+        else
+        {
+            throw new ArgumentException($"Invalid input - {input} is not valid");
+        }
+    }
+}
